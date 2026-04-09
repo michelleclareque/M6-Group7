4884f0f8e50a7c84dcab304a41060629b622fb50
michelleclareque <michelle.clare.que@student.ateneo.edu>
2026-04-09 17:55:49 +0800

    edited proj views

--- M6P/MyInventoryApp/views.py
+++ M6P/MyInventoryApp/views.py
@@ -1,18 +1,79 @@
-from django.shortcuts import render
-from .models import Supplier, WaterBottle
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Supplier, WaterBottle, Account
 
 # Create your views here.
-def view_bottles(request):
-    waterbottle_objects = WaterBottle.objects.all()
-    return render(request, 'MyInventoryApp/view_bottles.html', {'waterbottles':waterbottle_objects})
+def view_bottles(request, pk):
+    supplier_objects = get_object_or_404(Supplier, pk=pk)
+    waterbottle_objects = WaterBottle.objects.filter(supplier=supplier_objects)
+    return render(request, 'MyInventoryApp/view_bottles.html', {'waterbottles':waterbottle_objects, 'supplier':supplier_objects})
 
-def view_supplier(request):
+def view_supplier(request, pk):
     supplier_objects = Supplier.objects.all()
     return render(request, 'MyInventoryApp/view_supplier.html', {'suppliers':supplier_objects})
 
-def view_inventory(request):
-    #waterbottle_objects = WaterBottle.objects.all(), {'waterbottles':waterbottle_objects}
-    return render(request, 'MyInventoryApp/base.html')
-
 def add_bottle(request):
-    return render(request, 'MyInventoryApp/add_bottle.html')
+    if request.method=="POST":
+        suppliers = Supplier.objects.all()
+        name = request.POST.get('name')
+        volume = request.POST.get('volume')
+        supplier_id = request.POST.get('supplier')
+        supplier = Supplier.objects.get(pk=supplier_id)
+        WaterBottle.objects.create(name=name, volume=volume, supplier=supplier)
+        return redirect('view_supplier')
+    return render(request, 'MyInventoryApp/add_bottle.html', {'suppliers': suppliers})
+
+def login_view(request):
+    if request.method == "POST":
+        uname = request.POST.get("username")
+        pword = request.POST.get("password")
+        try:
+            acc = Account.objects.get(username=uname, password=pword)
+            return redirect("view_supplier", pk=acc.pk)
+        except Account.DoesNotExist:
+            return render(request, "MyInventoryApp/login.html", {"error": "Invalid login"})
+    return render(request, "MyInventoryApp/login.html")
+
+def signup_view(request):
+    if request.method == "POST":
+        uname = request.POST.get("username")
+        pword = request.POST.get("password")
+        if Account.objects.filter(username=uname).exists():
+            return render(request, "MyInventoryApp/signup.html", {"error": "Account already exists"})
+        Account.objects.create(username=uname, password=pword)
+        return render(request, "MyInventoryApp/login.html", {"success": "Account created successfully"})
+    return render(request, "MyInventoryApp/signup.html")
+
+def view_detail(request, pk):
+    d = get_object_or_404(WaterBottle, pk=pk)
+    return render(request, 'MyInventoryApp/view_detail.html', {'d': d})
+
+def manage_account(request, pk):
+    acc = get_object_or_404(Account, pk=pk)
+    return render(request, "MyInventoryApp/manage_account.html", {"account": acc})
+
+def change_password(request, pk):
+    acc = get_object_or_404(Account, pk=pk)
+    if request.method == "POST":
+        current_password = request.POST.get("current_password")
+        new_password = request.POST.get("new_password")
+        confirm_password = request.POST.get("confirm_password")
+        
+        if acc.password == current_password and new_password == confirm_password:
+            acc.password = new_password
+            acc.save()
+            return redirect("manage_account", pk=pk)
+        else:
+            return render(request, "MyInventoryApp/change_password.html", {"account": acc, "error": " Wrong Current Password or Confirm Passowed doesn't match"})
+    return render(request, "MyInventoryApp/change_password.html", {"account": acc})
+
+def delete_account(request, pk):
+    acc = get_object_or_404(Account, pk=pk)
+    acc.delete()
+    return redirect("login")
+
+def logout_view(request):
+    return redirect("login")
+
+def delete_bottle(request, pk):
+    WaterBottle.objects.filter(pk=pk).delete()
+    return redirect('view_bottles')
