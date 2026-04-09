c6e96dee0a688553e4882276738dc545991698e3
rsy <nathan.riley.sy@student.ateneo.edu>
2026-04-09 22:58:17 +0800

    login, signup, manageacc htmls edited, cert comments, coa added

--- M6P/MyInventoryApp/models.py
+++ M6P/MyInventoryApp/models.py
@@ -1,16 +1,19 @@
-"""
-We hereby attest to the truth of the following facts:
+# , ; Nathan Riley Sy, 244311; ,
+# April , 2026 
 
-We have not discussed the Python code in our program with anyone
+'''
+I hereby attest to the truth of the following facts:
+
+I have not discussed the Python language code in my program with anyone
 other than my instructor or the teaching assistants assigned to this course.
 
-We have not used Python code obtained from another student, or
-any other unauthorized source, whether modified or unmodified.
+I have not used Python language code obtained from another student, or
+any other unauthorized source, either modified or unmodified.
 
-If any Python code or documentation used in our program was
-obtained from another source, it has been clearly noted with citations in the
+If any Python language code or documentation used in my program was
+obtained from another source, such as a textbook or course notes, that has been clearly noted with proper citation in the
 comments of my program.
-"""
+'''
 
 from django.db import models
 from django.utils import timezone
